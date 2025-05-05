62a30a35636531e064a8e43b91705cfc6a54de1c
koichi.ikeda <koichiikeda.0111@gmail.com>
2025-05-05 19:04:18 +0900

    feat:画像を読み込んで表示しよう / ebiten.ImageとGPU

--- novel_game/main.go
+++ novel_game/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	_ "image/jpeg"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 type game struct {
+	bg     *ebiten.Image
+	person *ebiten.Image
+}
+
+func newGame() (*game, error) {
+	g := &game{}
+	img, _, err := ebitenutil.NewImageFromFile("assets/bg.jpg")
+	if err != nil {
+		return nil, err
+	}
+	g.bg = img
+	img, _, err = ebitenutil.NewImageFromFile("assets/person.png")
+	if err != nil {
+		return nil, err
+	}
+	g.person = img
+	return g, nil
 }
 
 func (g *game) Update() error {
@@ -13,17 +32,28 @@ func (g *game) Update() error {
 }
 
 func (g *game) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(float64(screen.Bounds().Dx())/float64(g.bg.Bounds().Dx()), float64(screen.Bounds().Dy())/float64(g.bg.Bounds().Dy()))
+	screen.DrawImage(g.bg, op)
+
+	op = &ebiten.DrawImageOptions{}
+	screen.DrawImage(g.person, op)
+
 	ebitenutil.DebugPrint(screen, "Hello, World!")
 }
 
 func (g *game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return outsideWidth / 2, outsideHeight / 2
+	return outsideWidth, outsideHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Ebitengine 入門")
-	if err := ebiten.RunGame(&game{}); err != nil {
+	ebiten.SetWindowSize(1280, 720)
+	ebiten.SetWindowTitle("ノベルゲーム")
+	g, err := newGame()
+	if err != nil {
+		panic(err)
+	}
+	if err := ebiten.RunGame(g); err != nil {
 		panic(err)
 	}
 }
