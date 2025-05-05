0c52a25646baa9f71d005ba5d578b1f05b3d0fbb
koichi.ikeda <koichiikeda.0111@gmail.com>
2025-05-05 11:34:34 +0900

    feat:キャラを動かそう / ゲームのフレームとその周辺

--- miniten/main.go
+++ miniten/main.go
@@ -2,10 +2,17 @@ package main
 
 import "github.com/eihigh/miniten"
 
+var y = 0
+
 func main() {
 	miniten.Run(draw)
 }
 
 func draw() {
-	miniten.DrawImage("gopher.png", 0, 0)
+	if miniten.IsClicked() {
+		y -= 6
+	} else {
+		y += 6
+	}
+	miniten.DrawImage("gopher.png", 0, y)
 }
