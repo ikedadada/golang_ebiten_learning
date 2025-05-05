29bc1fd2087f31e68218d2c5da1e1f509d69a6ea
koichi.ikeda <koichiikeda.0111@gmail.com>
2025-05-05 13:58:13 +0900

    はねるgopherくんゲームの見た目を作ろう

--- miniten/main.go
+++ miniten/main.go
@@ -6,18 +6,80 @@ import (
 	"github.com/eihigh/miniten"
 )
 
-var xs = []int{100, 200, 300}
+var (
+	y  = 0.0
+	vy = 0.0
+
+	frames = 0
+	walls  = []Wall{}
+)
 
 func main() {
 	miniten.Run(draw)
 }
 
 func draw() {
+	miniten.DrawImage("sky.png", 0, 0)
+
 	if miniten.IsClicked() {
-		xs = append(xs, rand.N(640))
+		vy = -6
 	}
-	for i := range xs {
-		xs[i] += 1
-		miniten.DrawImage("gopher.png", xs[i], 0)
+	vy += 0.5
+	y += vy
+
+	if y > 360 {
+		y = 360
 	}
+
+	miniten.DrawImage("gopher.png", 100, int(y))
+
+	frames += 1
+	if frames%100 == 0 {
+		walls = append(walls, NewWall())
+	}
+
+	for _, wall := range walls {
+		wall.Update()
+		wall.Draw()
+	}
+}
+
+type pipe struct {
+	x int
+	y int
+}
+
+type Wall struct {
+	topPipe    *pipe
+	bottomPipe *pipe
+}
+
+func NewWall() Wall {
+
+	holeY := rand.N(200)
+	holeHeight := 100
+
+	topPipe := pipe{
+		x: 640,
+		y: int(holeY - 360),
+	}
+	bottomPipe := pipe{
+		x: 640,
+		y: int(holeY + holeHeight),
+	}
+
+	return Wall{
+		topPipe:    &topPipe,
+		bottomPipe: &bottomPipe,
+	}
+}
+
+func (w *Wall) Draw() {
+	miniten.DrawImage("wall.png", w.topPipe.x, w.topPipe.y)
+	miniten.DrawImage("wall.png", w.bottomPipe.x, w.bottomPipe.y)
+}
+
+func (w *Wall) Update() {
+	w.topPipe.x -= 3
+	w.bottomPipe.x -= 3
 }
